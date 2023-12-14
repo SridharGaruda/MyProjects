164b055f9153adbf6bbe77b72a12506c5f67c204
SridharGaruda <sridhar.auminfo@gmail.com>
2023-12-14 17:57:48 +0530

    first project

---
+++ FirstProject/src/main/java/ITS/FirstProject/App.java
@@ -0,0 +1,13 @@
+package ITS.FirstProject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
